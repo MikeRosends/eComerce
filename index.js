705145aab59084aaddfd111b0551ca66d8835970
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-05-08 19:19:18 +0100

    products are inserted fully dynamicly in Home Page

--- index.js
+++ index.js
@@ -6,73 +6,88 @@ async function fetchProducts() {
   console.log("Number of items: " + product.length);
 
   // Containers
-  const proContainer = $("<div>", {
-    class: "pro",
-  });
-  const proDiv = $(".pro-container");
-  const descDiv = $("<div>", {
-    class: "des",
-  });
-  const starDiv = $("<div>", {
-    class: "star",
-  });
+  const productsContainer = (clazz) => $(`.${clazz}`);
+  const productCard = (id) =>
+    $("<div>", {
+      class: "pro",
+      id: `product-${id}`,
+    });
+  const descDiv = (id) =>
+    $("<div>", {
+      class: "des",
+      id: `product-desc-${id}`,
+    });
+  const starDiv = (id) =>
+    $("<div>", {
+      class: "star",
+      id: `product-star-${id}`,
+    });
 
   // Elements
-  const img = $("<img>", {
-    src: "img/products/f1.jpg",
-    onclick: "window.location.href='sproduct.html'",
-  });
+  const img = (id) =>
+    $("<img>", {
+      onclick: "window.location.href='sproduct.html'",
+      id: `product-img-${id}`,
+    });
   const starIcon = `<i class="fas fa-star"></i>`;
   const starAnchor = `<a href="#"><i class="fa-solid fa-star favorite"></i></a>`;
   const cartAnchor = `<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>`;
 
-
   // Function that generates each card
-  const generateProductContainers = function (num) {
+  const generateProductContainers = function (num, targetContainer) {
+    const productCardObj = productCard(num, targetContainer);
+    const starDivObj = starDiv(num);
+    const descDivObj = descDiv(num);
+    const imgObj = img(num);
+    // Append productCard to productsContainer
+    productCardObj.appendTo(productsContainer(targetContainer)[0]);
 
-    // Create a new div for each element
+    // Append img, and descriptionDiv to the productCard
+    const addImgAndDescDiv = () =>
+      productCardObj.append(imgObj, descDivObj, starAnchor, cartAnchor);
 
+    // Adding the source attribute to the image
+    const addIMG_path = (path) => imgObj.attr("src", path);
 
+    // Append starsDiv to the description div
+    const addStarDivToDescDiv = () => starDivObj.appendTo(descDivObj);
 
     // Appending stars to the starDiv
-    const addStars = function (num) {
-      descDiv.append(starDiv);
-      for (let i = 0; i < num; i++) {
-        starDiv.append(starIcon);
+    const addStarsToStarDiv = (num) => {
+      let i = 0;
+      for (i = 0; i < num; i++) {
+        starDivObj.append(starIcon);
       }
     };
 
-    // Appending description to the description div (descDiv)
-
-    const addDesc = function (brand, name, price) {
-      descDiv.append(`<span>${brand}</span>`, `<h5>${name}</h5>`);
-      addStars(product[num].rating);
-      descDiv.append(`<h4>$${price}</h4>`);
-    };
-    addDesc(product[num].brand, product[num].name, product[num].price);
-
-    // Adding elements to the product div
-    const createProdCard = function (
-      mainContainer,
-      secContainer,
-      img,
-      descCont,
-      button1,
-      button2
-    ) {
-      mainContainer.append(secContainer);
-      secContainer.append(img, descCont, button1, button2);
+    // Appending description to the description div (descDivObj)
+    const addDescriptionToDescDiv = (brand, name, price) => {
+      descDivObj.append(brand, name);
+      addStarDivToDescDiv();
+      addStarsToStarDiv(product[num].rating);
+      descDivObj.append(price);
     };
 
-    createProdCard(proDiv, proContainer, img, descDiv, starAnchor, cartAnchor);
+    // Calling the functions in the correct order
+    const generateCards = function () {
+      addIMG_path(`http://127.0.0.1:8080/img/products/f${num}.jpg`);
+      addImgAndDescDiv();
+      addDescriptionToDescDiv(
+        `<span>${product[num].brand}</span>`,
+        `<h5>${product[num].name}</h5>`,
+        `<h4>$${product[num].price}</h4>`
+      );
+    };
 
+    generateCards();
   };
 
-  let num = 0
+  // Incrementable number to load each product card
+  let num = 0;
 
-  for(let i = 0; i < product.length - 1; i++) {
-    num = num + 1;
-    generateProductContainers(num);
+  for (let i = 0; i < product.length; i++) {
+    generateProductContainers(num, "pro-container-1");
+    generateProductContainers(num++, "pro-container-2");
   }
 }
 
